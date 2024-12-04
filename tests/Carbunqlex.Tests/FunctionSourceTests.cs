e4fc5aa28aefec6421b2ef9fad854c2f6bce70e9
MSugiura <mk3008.develop@gmail.com>
2024-12-04 23:14:35 +0900

    Add unit tests for FunctionSource class methods

---
+++ tests/Carbunqlex.Tests/FunctionSourceTests.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using Carbunqlex.DatasourceExpressions;
+using Carbunqlex.QueryModels;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Carbunqlex.Tests;
+
+public class FunctionSourceTests(ITestOutputHelper output)
+{
+    private readonly ITestOutputHelper output = output;
+
+    [Fact]
+    public void ToSql_ShouldReturnCorrectSqlString()
+    {
+        // Arrange
+        var functionName = "TestFunction";
+        var arguments = new List<IValueExpression> { new ConstantExpression(1), new ConstantExpression(2) };
+        var alias = "TestAlias";
+        var columnAliases = new ColumnAliases(new List<string> { "col1", "col2" });
+        var functionSource = new FunctionSource(functionName, arguments, alias, columnAliases);
+
+        // Act
+        var sql = functionSource.ToSql();
+        output.WriteLine(sql);
+
+        // Assert
+        Assert.Equal("TestFunction(1, 2) AS TestAlias(col1, col2)", sql);
+    }
+}
