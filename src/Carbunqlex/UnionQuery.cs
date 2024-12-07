2be253b8accf840e0f7a14c822c256bc33dfb4c4
MSugiura <mk3008.develop@gmail.com>
2024-12-07 21:06:01 +0900

    Implemented UnionQuery class
    - Added support for combining multiple SelectQuery instances using UNION, UNION ALL, INTERSECT, and EXCEPT.
    - Included ToSql and GetLexemes methods to generate SQL strings and lexemes for UnionQuery.
    - Added tests for UnionQuery to ensure correct SQL generation for various union types and multiple queries.

---
+++ src/Carbunqlex/UnionQuery.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Carbunqlex.Clauses;
+
+public enum UnionType
+{
+    Union,
+    UnionAll,
+    Intersect,
+    Except
+}
+
+internal static class UnionTypeExtensions
+{
+    public static string ToSqlString(this UnionType unionType)
+    {
+        return unionType switch
+        {
+            UnionType.Union => "union",
+            UnionType.UnionAll => "union all",
+            UnionType.Intersect => "intersect",
+            UnionType.Except => "except",
+            _ => throw new ArgumentOutOfRangeException(nameof(unionType), unionType, null)
+        };
+    }
+}
+
+public class UnionQuery : IQuery
+{
+    public IQuery Left { get; }
+    public IQuery Right { get; }
+    public UnionType UnionType { get; }
+
+    public UnionQuery(IQuery left, IQuery right, UnionType unionType)
+    {
+        Left = left;
+        Right = right;
+        UnionType = unionType;
+    }
+
+    public string ToSql()
+    {
+        var sb = new StringBuilder();
+        sb.Append(Left.ToSql());
+        sb.Append(" ");
+        sb.Append(UnionType.ToSqlString());
+        sb.Append(" ");
+        sb.Append(Right.ToSql());
+        return sb.ToString();
+    }
+
+    public IEnumerable<Lexeme> GetLexemes()
+    {
+        var lexemes = new List<Lexeme>();
+        lexemes.AddRange(Left.GetLexemes());
+        lexemes.Add(new Lexeme(LexType.Keyword, UnionType.ToSqlString()));
+        lexemes.AddRange(Right.GetLexemes());
+        return lexemes;
+    }
+}
---
+++ tests/Carbunqlex.Tests/UnionQueryTests.cs
@@ -0,0 +1,147 @@
+﻿using Carbunqlex.Clauses;
+using Carbunqlex.ValueExpressions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Carbunqlex.Tests;
+
+public class UnionQueryTests
+{
+    private readonly ITestOutputHelper output;
+
+    public UnionQueryTests(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    private ColumnExpression CreateColumnExpression(string columnName)
+    {
+        return new ColumnExpression(columnName);
+    }
+
+    [Fact]
+    public void ToSql_WithUnion_ReturnsCorrectSql()
+    {
+        // Arrange
+        var selectClause1 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName1"))
+        );
+        var selectQuery1 = new SelectQuery(selectClause1);
+
+        var selectClause2 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName2"))
+        );
+        var selectQuery2 = new SelectQuery(selectClause2);
+
+        var unionQuery = new UnionQuery(selectQuery1, selectQuery2, UnionType.Union);
+
+        // Act
+        var sql = unionQuery.ToSql();
+        output.WriteLine(sql);
+
+        // Assert
+        Assert.Equal("select ColumnName1 union select ColumnName2", sql);
+    }
+
+    [Fact]
+    public void ToSql_WithUnionAll_ReturnsCorrectSql()
+    {
+        // Arrange
+        var selectClause1 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName1"))
+        );
+        var selectQuery1 = new SelectQuery(selectClause1);
+
+        var selectClause2 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName2"))
+        );
+        var selectQuery2 = new SelectQuery(selectClause2);
+
+        var unionQuery = new UnionQuery(selectQuery1, selectQuery2, UnionType.UnionAll);
+
+        // Act
+        var sql = unionQuery.ToSql();
+        output.WriteLine(sql);
+
+        // Assert
+        Assert.Equal("select ColumnName1 union all select ColumnName2", sql);
+    }
+
+    [Fact]
+    public void ToSql_WithIntersect_ReturnsCorrectSql()
+    {
+        // Arrange
+        var selectClause1 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName1"))
+        );
+        var selectQuery1 = new SelectQuery(selectClause1);
+
+        var selectClause2 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName2"))
+        );
+        var selectQuery2 = new SelectQuery(selectClause2);
+
+        var unionQuery = new UnionQuery(selectQuery1, selectQuery2, UnionType.Intersect);
+
+        // Act
+        var sql = unionQuery.ToSql();
+        output.WriteLine(sql);
+
+        // Assert
+        Assert.Equal("select ColumnName1 intersect select ColumnName2", sql);
+    }
+
+    [Fact]
+    public void ToSql_WithExcept_ReturnsCorrectSql()
+    {
+        // Arrange
+        var selectClause1 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName1"))
+        );
+        var selectQuery1 = new SelectQuery(selectClause1);
+
+        var selectClause2 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName2"))
+        );
+        var selectQuery2 = new SelectQuery(selectClause2);
+
+        var unionQuery = new UnionQuery(selectQuery1, selectQuery2, UnionType.Except);
+
+        // Act
+        var sql = unionQuery.ToSql();
+        output.WriteLine(sql);
+
+        // Assert
+        Assert.Equal("select ColumnName1 except select ColumnName2", sql);
+    }
+
+    [Fact]
+    public void ToSql_WithThreeQueries_ReturnsCorrectSql()
+    {
+        // Arrange
+        var selectClause1 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName1"))
+        );
+        var selectQuery1 = new SelectQuery(selectClause1);
+
+        var selectClause2 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName2"))
+        );
+        var selectQuery2 = new SelectQuery(selectClause2);
+
+        var selectClause3 = new SelectClause(
+            new SelectExpression(CreateColumnExpression("ColumnName3"))
+        );
+        var selectQuery3 = new SelectQuery(selectClause3);
+
+        var unionQuery1 = new UnionQuery(selectQuery1, selectQuery2, UnionType.Union);
+        var unionQuery2 = new UnionQuery(unionQuery1, selectQuery3, UnionType.UnionAll);
+
+        // Act
+        var sql = unionQuery2.ToSql();
+        output.WriteLine(sql);
+
+        // Assert
+        Assert.Equal("select ColumnName1 union select ColumnName2 union all select ColumnName3", sql);
+    }
+}
