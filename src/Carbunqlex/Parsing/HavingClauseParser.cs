5bd9a36595e41122c05d87c8e50776ddf0a5840a
MSugiura <mk3008.develop@gmail.com>
2025-01-31 22:06:31 +0900

    Adding HavingClauseParser and Tests

---
+++ src/Carbunqlex/Parsing/HavingClauseParser.cs
@@ -0,0 +1,17 @@
+﻿using Carbunqlex.Clauses;
+
+namespace Carbunqlex.Parsing;
+
+public class HavingClauseParser
+{
+    private static string ParserName => nameof(HavingClauseParser);
+
+    public static HavingClause Parse(SqlTokenizer tokenizer)
+    {
+        tokenizer.Read(ParserName, "having");
+
+        var expression = ValueExpressionParser.Parse(tokenizer);
+
+        return new HavingClause(expression);
+    }
+}
---
+++ tests/Carbunqlex.Tests/ParsingTests/HavingClauseParserTests.cs
@@ -0,0 +1,40 @@
+﻿using Carbunqlex.Parsing;
+using Xunit.Abstractions;
+
+namespace Carbunqlex.Tests.ParsingTests;
+
+public class HavingClauseParserTests
+{
+    public HavingClauseParserTests(ITestOutputHelper output)
+    {
+        Output = output;
+    }
+
+    private ITestOutputHelper Output { get; }
+
+    [Fact]
+    public void Parse_WithSingleColumn_ReturnsCorrectSql()
+    {
+        // Arrange
+        var tokenizer = new SqlTokenizer("having a.id = 1");
+        // Act
+        var result = HavingClauseParser.Parse(tokenizer);
+        Output.WriteLine(result.ToSqlWithoutCte());
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("having a.id = 1", result.ToSqlWithoutCte());
+    }
+
+    [Fact]
+    public void Parse_WithAndOperator_ReturnsCorrectSql()
+    {
+        // Arrange
+        var tokenizer = new SqlTokenizer("having a.id = 1 and b.value = 2");
+        // Act
+        var result = HavingClauseParser.Parse(tokenizer);
+        Output.WriteLine(result.ToSqlWithoutCte());
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("having a.id = 1 and b.value = 2", result.ToSqlWithoutCte());
+    }
+}
