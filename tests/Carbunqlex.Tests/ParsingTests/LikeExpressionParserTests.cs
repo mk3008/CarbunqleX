c102207383debceb10b60fe9d929f25ac4e2088b
MSugiura <mk3008.develop@gmail.com>
2025-02-01 22:18:42 +0900

    Add test for LIKE expression with escape option

--- tests/Carbunqlex.Tests/ParsingTests/LikeExpressionParserTests.cs
+++ tests/Carbunqlex.Tests/ParsingTests/LikeExpressionParserTests.cs
@@ -48,4 +48,28 @@ public class LikeExpressionParserTests
         Assert.Equal("column", ((LikeExpression)result).Left.ToSqlWithoutCte());
         Assert.Equal("'a%'", ((LikeExpression)result).Right.ToSqlWithoutCte());
     }
+
+    //escape option
+    [Fact]
+    public void Parse_LikeExpressionWithEscapeOption_ReturnsCorrectExpression()
+    {
+        // Arrange
+        var tokenizer = new SqlTokenizer("""
+            'hello_world' like 'hello\_world' escape '\'
+            """);
+        // Act
+        var result = ValueExpressionParser.Parse(tokenizer);
+        Output.WriteLine(result.ToSqlWithoutCte());
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<LikeExpression>(result);
+        Assert.False(((LikeExpression)result).IsNegated);
+        Assert.Equal("'hello_world'", ((LikeExpression)result).Left.ToSqlWithoutCte());
+        Assert.Equal("""
+            'hello\_world'
+            """, ((LikeExpression)result).Right.ToSqlWithoutCte());
+        Assert.Equal("""
+            '\'
+            """, ((LikeExpression)result).EscapeOption);
+    }
 }
