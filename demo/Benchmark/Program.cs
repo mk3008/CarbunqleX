1dfc8aef9022e2ff8b8db295b68fa6a3a482c584
MSugiura <mk3008.develop@gmail.com>
2025-02-08 15:29:42 +0900

    Adding benchmark project and updating related settings
    Added a new project `Benchmark` to the `Carbunqlex.sln` file.

---
+++ demo/Benchmark/Program.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Carbunql;
+using Carbunqlex.Parsing;
+using SqModel.Analysis;
+
+public class Program
+{
+    public static void Main()
+    {
+        BenchmarkRunner.Run<SimpleSelectQueryParseBenchmark>();
+    }
+}
+
+public class SimpleSelectQueryParseBenchmark
+{
+    private readonly string QueryString = "select id, name from users";
+
+    [Benchmark]
+    public string SqModel()
+    {
+        var sq = SqlParser.Parse(QueryString);
+        return sq.ToQuery().CommandText;
+    }
+
+    [Benchmark]
+    public string Carbunql()
+    {
+        var sq = new Carbunql.SelectQuery(QueryString);
+        return sq.ToOneLineText();
+    }
+
+    [Benchmark]
+    public string Carbunqlex()
+    {
+        var tokenizer = new SqlTokenizer(QueryString);
+        var sq = SelectQueryParser.Parse(tokenizer);
+        return sq.ToSql();
+    }
+}
