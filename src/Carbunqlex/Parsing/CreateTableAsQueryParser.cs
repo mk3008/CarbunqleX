a167a9a3ca45dd396d452fb0a03dc8e0dd8757fb
MSugiura <mk3008.develop@gmail.com>
2025-02-16 13:49:16 +0900

    Add CreateTableAsQuery class and parser

---
+++ src/Carbunqlex/Parsing/CreateTableAsQueryParser.cs
@@ -0,0 +1,25 @@
+﻿namespace Carbunqlex.Parsing;
+
+
+public class CreateTableAsQueryParser
+{
+    public static CreateTableAsQuery Parse(string sql)
+    {
+        var tokenizer = new SqlTokenizer(sql);
+        return Parse(tokenizer);
+    }
+
+    public static CreateTableAsQuery Parse(SqlTokenizer tokenizer)
+    {
+        var command = tokenizer.Read(["create table", "create temporary table"]);
+        var isTemporary = command.CommandOrOperatorText == "create temporary table";
+
+        var tableSource = TableSourceParser.Parse(tokenizer);
+
+        tokenizer.Read("as");
+
+        var selectQuery = SelectQueryParser.Parse(tokenizer);
+
+        return new CreateTableAsQuery(tableSource, selectQuery, isTemporary);
+    }
+}
